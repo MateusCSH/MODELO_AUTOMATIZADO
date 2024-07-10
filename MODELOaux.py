bff17993e7d9fd57fb14f8a452d35d2a8ea87923
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-07-10 16:20:55 -0300

    Update MODELOaux.py

--- MODELOaux.py
+++ MODELOaux.py
@@ -9,22 +9,20 @@ from Newprecess_mes import selec_mes
 
 def redutora():
         link = 'https://docs.google.com/spreadsheets/d/1CiRe1cbT6MVjp5j_v2S3-EYvZNLTyFWjKBua9DCx3xc/edit#gid=1410983719'
-        subprocess.run(r'C:\Program Files\Google\Chrome\Application\chrome')
-
+        subprocess.run(r'C:\Users\Olá\AppData\Local\Programs\Opera\opera.exe')
+        
         time.sleep(2)
+
         pyautogui.write(link)
+        time.sleep(1.5)
         pyautogui.press('enter')
         time.sleep(1.5)
 
-
-
 def repetição(df, click_nome):
-        
         pyautogui.click(click_nome.x, click_nome.y)
         pyautogui.press('down')
 
         pyautogui.PAUSE = 0.2
-        
         for lin in df.index:
                 nome = df.loc[lin, 'Nome'].upper()
                 pyautogui.write(nome)
@@ -51,4 +49,4 @@ def repetição(df, click_nome):
                 pyautogui.press('down')
 
         pyautogui.scroll(500)
-        time.sleep(1)       
+        time.sleep(1)
